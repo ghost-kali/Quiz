52ce81533d653392bade2166aed1ae8e8869bc0b
ghost-kali <109295912+ghost-kali@users.noreply.github.com>
2024-08-14 09:14:35 +0530

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,34 @@
+from flask import Flask ,jsonify,render_template,request
+from flask_cors import CORS
+from main import get_ticket_response_pydantic
+
+query = """
+Provide me 5 Questions on Python based on beginner Level Understanding
+"""
+
+# --------------------------------------------------------------
+# Providing a JSON Schema
+# --------------------------------------------------------------
+
+
+
+
+app = Flask( __name__) 
+CORS(app)
+#@app.get("/")
+#def index_get():
+ #   return render_template("base.html")
+
+@app.get("/predict")
+def predict():
+    
+    message = get_ticket_response_pydantic(query=query)
+
+
+    return message
+
+
+if   __name__ == "__main__" : 
+    predict()
+    app.run(debug=True)
+  
