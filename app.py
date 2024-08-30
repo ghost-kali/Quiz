eacb8b07af319977adef2da1a04dc00a581abf0b
ghost-kali <109295912+ghost-kali@users.noreply.github.com>
2024-08-30 11:19:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,59 @@
-from flask import Flask ,jsonify,render_template,request
-from flask_cors import CORS
-from main import get_ticket_response_pydantic
 
-query = """
-Provide me 5 Questions on Python based on beginner Level Understanding
+
+from openai import OpenAI
+from pydantic import BaseModel, Field
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+MODEL = "gpt-4o-2024-08-06"
+
+
+system_prompt = """
+You are a AI Quiz Bot. You will be provided with a question,
+and 4 options, including the answer for the questions.Never provide the first option as answer,
+And you will generate question based on difficulty level 1 to 20
 """
 
+
+
+
 # --------------------------------------------------------------
-# Providing a JSON Schema
+# Using Pydantic
 # --------------------------------------------------------------
 
 
+class TicketResolution(BaseModel):
+    class QuizQuestion(BaseModel):
+        question: str = Field(description="question on the required topic.")
+        options: list[str] = Field(description="randomized list of options")
+        answer : str  = Field(description="Answer of the question")
 
-
-app = Flask( __name__) 
-CORS(app)
-#@app.get("/")
-#def index_get():
- #   return render_template("base.html")
-
-@app.post("/predict")
-def predict():
-    data = request.get_json()
-    language = data.get('Language')
-    level = data.get('Level')
-    print(language)
-    print(level)
+    questions: list[QuizQuestion]
     
-    # Use the language and level parameters as needed in your function
-    
-    message = get_ticket_response_pydantic(language=language , level=level)
-    
-    return message
+    confidence: float = Field(description="Confidence in the resolution (0-1)")
 
 
+def get_ticket_response_pydantic(language: str , level : str):
+    query = f"""
+Provide me 5 Questions on {language} based on  Level {level} Understanding
+"""
 
-if   __name__ == "__main__" : 
-  
-    app.run(debug=True)
-  
+    completion = client.beta.chat.completions.parse(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": query},
+        ],
+        response_format=TicketResolution,
+    )
+
+    response_pydantic =  completion.choices[0].message.parsed
+    response = response_pydantic.model_dump()
+    return((response))
+
+
+if __name__ == "__main__":
+    print(get_ticket_response_pydantic(language='java'))
